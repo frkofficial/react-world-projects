d15835a587b865bc6bbabcdfa75e0c5596df0e7e
frkofficial <farukakram2000@gmail.com>
2025-09-20 17:02:25 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Countries from './components/countries/countries'
+
+function App() {
+ 
+
+  return (
+    <>
+      <h1>React World On the go...</h1>
+      <Countries></Countries>
+      
+    </>
+  )
+}
+
+export default App
---
+++ src/components/countries/countries.jsx
@@ -0,0 +1,13 @@
+
+
+import React from 'react';
+
+const Countries = () => {
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Countries;
